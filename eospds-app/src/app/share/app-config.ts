f770efd2277606d196d0ce40fa9697ee6dd4429b
Pinkcheng <hef19970210@gmail.com>
2021-06-25 21:54:39 +0800

    更換系統主題色2，修改連線IP

--- eospds-app/src/app/share/app-config.ts
+++ eospds-app/src/app/share/app-config.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AppConfig {
-  public readonly apiUrl = 'http://10.10.105.11:9487';
+  public readonly apiUrl = 'http://140.125.45.125:9487';
   public readonly apiVersion = '/api/v1.0';
   public readonly apiOptions = { headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded') };
 }
