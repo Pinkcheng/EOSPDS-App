c4ef7e4e7553836d9b4e9c9b688367412da78a45
Pinkcheng <hef19970210@gmail.com>
2021-06-26 19:09:50 +0800

    修改domain

--- eospds-app/src/app/share/app-config.ts
+++ eospds-app/src/app/share/app-config.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AppConfig {
-  public readonly apiUrl = 'http://140.125.45.125:9487';
+  public readonly apiUrl = 'https://eoslab.tw';
   public readonly apiVersion = '/api/v1.0';
   public readonly apiOptions = { headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded') };
 }
